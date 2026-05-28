7645ed0e425b5cd8d2e1e5cdb6ea0115e237a4b5
masjiddzikir <dzikirmasjid@gmail.com>
2026-05-28 08:23:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// =====================================================
+// FILE : script.js
+// =====================================================
+
+// JAM DIGITAL
 function updateClock(){
 
 const now = new Date();
@@ -18,3 +23,39 @@ h + ":" + m + ":" + s;
 setInterval(updateClock,1000);
 
 updateClock();
+
+
+// ============================
+// COUNTDOWN IQOMAH
+// ============================
+
+function startCountdown(id, minutes){
+
+let time = minutes * 60;
+
+setInterval(() => {
+
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
+
+    min = min < 10 ? "0"+min : min;
+    sec = sec < 10 ? "0"+sec : sec;
+
+    document.getElementById(id).innerHTML =
+    min + ":" + sec;
+
+    if(time > 0){
+        time--;
+    }
+
+},1000);
+
+}
+
+// UBAH ANGKA MENIT SESUAI KEINGINAN
+
+startCountdown("subuh-count",10);
+startCountdown("dzuhur-count",15);
+startCountdown("ashar-count",10);
+startCountdown("maghrib-count",5);
+startCountdown("isya-count",10);
